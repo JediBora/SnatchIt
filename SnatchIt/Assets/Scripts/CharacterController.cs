4ae3c4213592528eba59e04e0136ebda6adada61
JediBora <45404728+JediBora@users.noreply.github.com>
2020-01-24 18:41:43 -0500

    Created Character Controller

---
+++ SnatchIt/Assets/Scripts/CharacterController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+    public float speed;
+    Rigidbody2D rb;
+    Vector2 movement;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+    private void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+    }
+
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * speed * Time.fixedDeltaTime);
+    }
+}
