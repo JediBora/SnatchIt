319edb0e40de96811d7da3ba61b63f79d993b16a
Sawkman <58488161+Sawkman@users.noreply.github.com>
2020-04-02 19:09:02 -0400

    Added Audio
    Added audio files, listener, source and functionality in the code. The step sound will play when the player moves using the input axis

--- SnatchIt/Assets/Scripts/CharacterController.cs
+++ SnatchIt/Assets/Scripts/CharacterController.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class CharacterController : MonoBehaviour
 {
+    AudioSource audioData;
     public float speed;
     Rigidbody2D rb;
     Vector2 movement;
@@ -11,15 +12,29 @@ public class CharacterController : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        audioData = GetComponent<AudioSource>();
     }
     private void Update()
     {
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
+
+        if (Input.GetButtonDown("Horizontal")|| Input.GetButtonDown("Vertical"))
+        {
+            Debug.Log("Sound off!");
+            audioData.Play();
+        }
+
+        if(Input.GetButtonUp("Horizontal") || Input.GetButtonUp("Vertical"))
+        {
+            audioData.Stop();
+        }
     }
 
     void FixedUpdate()
     {
         rb.MovePosition(rb.position + movement * speed * Time.fixedDeltaTime);
     }
+
+    
 }
