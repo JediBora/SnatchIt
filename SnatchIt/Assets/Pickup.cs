4def55839baff7da453bfd5ea8e668a3b0cc6cb3
AdamQuiz <57140431+AdamQuiz@users.noreply.github.com>
2020-04-02 17:36:22 -0400

    Pickup Implemented under Sample Scene

---
+++ SnatchIt/Assets/Pickup.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pickup : MonoBehaviour
+{
+    private bool pickUpAllowed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (pickUpAllowed && Input.GetKeyDown(KeyCode.E))
+
+            PickupItem();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.name.Equals("Player"))
+        {
+            pickUpAllowed = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.name.Equals("Player"))
+        {
+            pickUpAllowed = false;
+        }
+    }
+
+    private void PickupItem()
+    {
+        Destroy(gameObject);
+    }
+}
